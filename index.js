d21b5a38b49cebf9cf471afaca3b93c17121bea8
qux-bbb <1147635419@qq.com>
2020-10-26 02:24:03 +0800

    heroku test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -118,7 +122,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
